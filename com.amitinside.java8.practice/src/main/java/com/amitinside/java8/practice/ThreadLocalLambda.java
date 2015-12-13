7ae651423c6958fca90639895c1c9db0957263b6
Amit Kumar Mondal <admin@amitinside.com>
2015-12-13 06:00:38 +0100

    Copyright Info Changed

--- com.amitinside.java8.practice/src/main/java/com/amitinside/java8/practice/ThreadLocalLambda.java
+++ com.amitinside.java8.practice/src/main/java/com/amitinside/java8/practice/ThreadLocalLambda.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2015 OSGi Alliance
+ * Copyright 2015 Amit Kumar Mondal
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
