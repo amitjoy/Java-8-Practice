79b5a910856b1399265a3e1f397aac03306b8fb2
Amit Kumar Mondal <admin@amitinside.com>
2015-12-16 02:51:09 +0100

    Added Collectors Partition Example

---
+++ com.amitinside.java8.practice/src/main/java/com/amitinside/java8/practice/CollectorsPartitionExample.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright 2015 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.amitinside.java8.practice;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.amitinside.java8.practice.model.Artist;
+
+public final class CollectorsPartitionExample {
+
+	public Map<Boolean, List<Artist>> bandsAndSolo(final Stream<Artist> artists) {
+		return artists.collect(Collectors.partitioningBy(Artist::isSolo));
+	}
+
+}
