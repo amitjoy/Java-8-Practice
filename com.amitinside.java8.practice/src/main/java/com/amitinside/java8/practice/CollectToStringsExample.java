f49ab5ce42aef91ef3e677728bd535cd75bc4376
Amit Kumar Mondal <admin@amitinside.com>
2015-12-17 01:26:09 +0100

    Added Collector String Joining Example

---
+++ com.amitinside.java8.practice/src/main/java/com/amitinside/java8/practice/CollectToStringsExample.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright 2015 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.amitinside.java8.practice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.amitinside.java8.practice.model.Artist;
+
+public final class CollectToStringsExample {
+
+	public final String formatArtistNames(final List<Artist> artists) {
+		return artists.stream().map(Artist::getName).collect(Collectors.joining(", ", "[", "]"));
+	}
+}
