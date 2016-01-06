e5cbba7be6ecc55ce52d76c5e9774322bb41a182
Amit Kumar Mondal <admin@amitinside.com>
2016-01-06 09:12:57 +0100

    jOOL Collectors

--- com.amitinside.java8.practice/src/main/java/com/amitinside/java8/practice/SeqJoolPractice.java
+++ com.amitinside.java8.practice/src/main/java/com/amitinside/java8/practice/SeqJoolPractice.java
@@ -15,7 +15,16 @@
  *******************************************************************************/
 package com.amitinside.java8.practice;
 
+import static org.jooq.lambda.Agg.allBy;
+import static org.jooq.lambda.Agg.max;
+import static org.jooq.lambda.Agg.maxBy;
+import static org.jooq.lambda.Agg.min;
+import static org.jooq.lambda.Agg.minBy;
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.jooq.lambda.Seq;
 
@@ -24,6 +33,13 @@ public final class SeqJoolPractice {
 	public static void main(final String[] args) {
 		final List<String> alphabet = Seq.rangeClosed('A', 'Z').map(Object::toString).toList();
 		final String[] strings = Seq.rangeClosed('a', 'z').map(Object::toString).toArray(String[]::new);
+
+		assertEquals(Optional.of(1), Seq.of(1, 2, 3).collect(min()));
+		assertEquals(Optional.of(3), Seq.of(1, 2, 3).collect(max()));
+		assertEquals(Optional.of(3), Seq.of(1, 2, 3).collect(minBy(i -> -i)));
+		assertEquals(Optional.of(1), Seq.of(1, 2, 3).collect(maxBy(i -> -i)));
+
+		System.out.println(Stream.of(1, 2, 3, 4).collect(allBy(num -> (num % 2) == 0)));
 		System.out.println(alphabet);
 		System.out.println(strings);
 	}
